b8a9e0dfa480cc97acaa77aa142d3357b8e91075
Abir Mahamud <mr.abirmahamud@gmail.com>
2021-09-19 13:39:01 +0600

    first commit

---
+++ odoo_template/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo Template',
+    'version': '14.0.1.0.0',
+    'summary': """Odoo Template""",
+    'description': """Odoo Template """,
+    'category': 'Generic Modules/Odoo Template',
+    'author': 'Farhan Tanvir, Abir Mahamud',
+    'company': 'Farhan Tanvir, Abir Mahamud',
+    'website': "https://www.farhantanvir.com",
+    'depends': ['web',
+                'hr'
+                ],
+    'data': [
+    ],
+    'images': ['static/description/banner.gif'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
